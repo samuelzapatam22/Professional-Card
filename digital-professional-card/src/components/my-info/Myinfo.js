a0d2b27f1bef3f2460ff25d12f4b8250c8a22d86
samuelzapatam22 <samuelzapatam1@gmail.com>
2024-01-31 13:52:16 -0500

    feat(components): adding components - adding Avatar component - adding Myinfo component - adding Socialnetwork component - adding Footer component - adding Aboutme component

---
+++ digital-professional-card/src/components/my-info/Myinfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Myinfo.css'
+const Myinfo = () => {
+    return (
+      <div className='Myinfo'>
+        <h1>Samuel Zapata Montoya</h1>
+        <h2>Junior Developer</h2>
+      </div>
+    );
+  }
+  
+  export default Myinfo;
